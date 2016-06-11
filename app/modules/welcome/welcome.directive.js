de2b642358399c1047595212d9cd9660586400c1
sevenmaster <seven-master@hotmail.de>
2016-06-11 11:50:20 +0200

    sorted

--- app/modules/welcome/welcome.directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('tickethunt-welcome', [])
-
-.directive('welcome', function() {
-    return {
-        restrict: 'E',
-        templateUrl: "/app/modules/welcome/template.html" 
-    };
-});
