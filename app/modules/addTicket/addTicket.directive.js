617ec01073224445d64d363a33a43c10cca89346
ebosseck <eric.bossecker@gmail.com>
2016-06-11 16:37:47 +0200

    addTicket3

--- app/modules/addTicket/addTicket.directive.js
+++ app/modules/addTicket/addTicket.directive.js
@@ -7,10 +7,18 @@ angular.module('addTicketTickethunt',[])
         controller: function ($scope) {
             $scope.getLocation = function () {
                  if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(showPosition);
+                    navigator.geolocation.getCurrentPosition($scope.fillCoords);
                 } else { 
-                  x.innerHTML = "Geolocation is not supported by this browser.";
+                 alert ("Geolocation is not supported by this browser.");
                 }
+            }
+
+            $scope.fillCoords = function (position) {
+                var x = document.getElementById("coordsX");
+                var y = document.getElementById("coordsY");
+
+                x.value = position.coords.latitude;
+                y.value = position.coords.longitude;
             }
 
             $scope.submitTicket = function () {
