e00ec63d1cd83111f290891717a3def7f9df9c7d
jasle <jas.le@web.de>
2016-06-11 21:15:08 +0200

    Add first service for API to backend

--- app/app.js
+++ app/app.js
@@ -11,10 +11,12 @@ angular.module('tickethunt', ['ngAnimate','mapTickethunt','addTicketTickethunt',
     "use strict";
 
     return $resource(
-        'https://tmp.pajowu.de/api/ticket/:id?format=json&:location',
+        'https://tmp.pajowu.de/api/ticket/:id?format=json&:location&:ticket_type&:min_valid',
         {
-            id: "@id",
-            location: "@min_lat,@max_lat,@min_lon,@max_lon"
+            id: "@id",//ID of the Tickets
+            location: "@min_lat,@max_lat,@min_lon,@max_lon",//Bounding Box search
+            ticket_type: "@type",//Type of the Tickets, get avaible types from 'TicketType'
+            min_valid: "@min_valid"//minimal remaining validity time, in minutes
         },{
             get: {method: 'GET'},
             delete: {method: 'DELETE'},
@@ -23,4 +25,20 @@ angular.module('tickethunt', ['ngAnimate','mapTickethunt','addTicketTickethunt',
 
 
 )
-);
+)
+/*
+.factory('TicketType', ['$resource', function ($resource) {
+    "use strict";
+
+    return $resource(
+        'https://tmp.pajowu.de/api/tickettype/?format=json&:',
+        {
+            id: "@id",
+        },{
+            get: {method: 'GET'},
+            post: {method: 'POST'}
+        }
+
+
+)
+)*/;
