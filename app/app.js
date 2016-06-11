e07c959180884051084ba08a9036346a504d67ff
ebosseck <eric.bossecker@gmail.com>
2016-06-11 15:44:19 +0200

    AddTicket2

--- app/app.js
+++ app/app.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-angular.module('tickethunt', ['ngAnimate','mapTickethunt','addTicketTickethunt'])
-=======
-angular.module('tickethunt', ['ngAnimate','mapTickethunt', 'claimTickethunt'])
->>>>>>> 38e2c913ce0dd0ea3d29fb59e3f43fbf9b72732c
+
+angular.module('tickethunt', ['ngAnimate','mapTickethunt','addTicketTickethunt', 'claimTickethunt'])
 
 .controller('mainCtrl', ['$scope', function($scope) {
     $scope.welcome = true;
